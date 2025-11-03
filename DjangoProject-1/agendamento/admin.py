0ead57dc0da551166d733b566e9c1b8695ca8680
iagoramon-max <iagoramon@gmail.com>
2025-11-02 21:53:16 -0300

    Update admin.py

--- DjangoProject-1/agendamento/admin.py
+++ DjangoProject-1/agendamento/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profissional, Servico, Agendamento
+from .models import Profissional, Servico, Agendamento, Cliente 
 
 # Register your models here.
 admin.site.register(Profissional)
 admin.site.register(Servico)
 admin.site.register(Agendamento)
+admin.site.register(Cliente)
